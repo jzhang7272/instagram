149c64aa850bea6ebfc6e400f15236fb55536bb9
Josey Zhang <jzhang7@fb.com>
2021-07-06 10:05:58 -0500

    created initial files

---
+++ instagram/instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  instagram
+//
+//  Created by Josey Zhang on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
